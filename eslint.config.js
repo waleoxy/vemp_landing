bcb70fd87dd64c50111e41361a87f9de627fe700
Olawale Otubu <waleoxy@gmail.com>
2024-05-11 21:07:57 +0100

    add:prettier

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
   pluginReactConfig,
   {
     ignores: [".eslintrc.cjs", "vite.config.ts"],
@@ -13,6 +14,11 @@ export default [
   {
     files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     rules: { "react/react-in-jsx-scope": "off", "react/jsx-uses-react": "off" },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
   { languageOptions: { globals: globals.browser } },
 ];
