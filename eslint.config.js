391e6b786a9928c4efefe24865f061b8e2225cb1
Olawale Otubu <waleoxy@gmail.com>
2024-05-11 19:09:18 +0100

    eslint.config modified

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,16 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  {
+    ignores: [".eslintrc.cjs", "vite.config.ts"],
+  },
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    rules: { "react/react-in-jsx-scope": "off", "react/jsx-uses-react": "off" },
+  },
+  { languageOptions: { globals: globals.browser } },
 ];
